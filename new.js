71102eb253bc884e1132672701db0198e030606f
JunHui <aakira@gmail.com>
2023-06-01 16:08:15 +0800

    删除js文件若干。。。

--- new.js
+++
@@ -1,9 +0,0 @@
-(function addPrint() {
-  var print = function(str) {
-    let body = document.querySelector('body')
-    let p = document.createElement('P')
-    p.innerText = str
-    body.appendChild(p)
-  }
-  window.print = print
-})()
--- printUtil.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  window.print = function () {
-    let body = document.querySelector('body')
-    let p = document.createElement('P')
-    p.innerText = str
-    body.appendChild(p)
-  }
-})()
